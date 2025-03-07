59979e8f7a23b3a75ff1ad1c1a08faf5dfe8fd72
Oleksandr Sirenko <oleksa.sirenko@gmail.com>
2025-03-07 21:29:11 +0200

    feat(preprocessing): expose preprocessing module interface

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,1 +1,13 @@
 """Preprocessing modules for tariff analyzer."""
+
+from .processor import TariffProcessor
+from .normalizer import CountryNormalizer, DateNormalizer
+from .deduplicator import ArrayBitMasker, EventDeduplicator
+
+__all__ = [
+    "TariffProcessor",
+    "CountryNormalizer",
+    "DateNormalizer",
+    "ArrayBitMasker",
+    "EventDeduplicator",
+]
